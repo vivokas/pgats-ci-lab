2100144d392d7ff5cc6c7bec1510dfc9c212f7a0
Vivokas <vivianevinici@gmail.com>
2025-06-08 11:31:43 -0300

    alterando arquivo e2e

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -11,7 +11,7 @@ test.describe(`user rides`, async () => {
     await page.getByLabel('Amount of people').selectOption('2');
     await page.getByRole('button', { name: 'Next' }).click();
 
-    expect(page.url()).toContain(`#ride/swings`);
+    expect(page.url()).toContain(`#success`);
   });
 
   test('user above height should not be allowed', async ({ page }) => {
